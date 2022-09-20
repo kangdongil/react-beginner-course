2ea7835c435e5bc2e66347db7c1ff933a79c97a0
kangdongil <akang8150@naver.com>
2022-09-20 14:53:49 +0000

    7.1 React ToDo List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import  { useState } from "react";
 
 function App() {
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if(toDo === "") {
+			return;
+		}
+		setToDo("");
+		setToDos((prevArr) => [toDo, ...prevArr]);
+	};
 	return (
-		<div></div>
+		<div>
+			<h1>My To Dos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={toDo}
+					onChange={onChange}
+					type="text"
+					placeholder="Write your ToDo"
+				/>
+				<button>Add ToDo</button>
+			</form>
+			<hr />
+			<ul>
+				{toDos.map(
+					(toDo, index) => <li key={index}>{toDo}</li>
+				)}
+			</ul>
+		</div>
 	);
 }
 
